40cc04c24c0a625150c71bcc349de1ace75df1a1
Vi Tran <vibro2006@gmail.com>
2025-12-11 14:32:52 -0800

    INIT

---
+++ FinalProject_Pacman/Source.cpp
@@ -0,0 +1,7 @@
+#include "Utils.hpp"
+
+
+int main()
+{
+    
+}
---
+++ FinalProject_Pacman/Utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#define LOG(x) std::cout << x
+#define LOG_LN(x) std::cout << x << std::endl
+#define LOG_EMPTY_LN() LOG("")
